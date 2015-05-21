c8cdca11e7d975a3cd3ba682df9ea30f38c9661a
William <william@shallum.net>
2015-05-21 14:40:14 +0000

    create entry model

--- be/django/phonebook/phonebook_be/tests.py
+++ be/django/phonebook/phonebook_be/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
 import json
+import urlparse
+from .models import Entry
+
+
+class EntryTests(TestCase):
+
+    def test_create_and_get_entry(self):
+        """Test creating Entry"""
+        entry = Entry.objects.create(name='Joe')
+        entry2 = Entry.objects.get(id=entry.id)
+        self.assertEquals(entry.name, entry2.name)
 
 
 class EntriesListTests(TestCase):
@@ -12,8 +23,15 @@ class EntriesListTests(TestCase):
         json.loads(response.content)
 
     def test_entries_post_to_create(self):
-        """Test POST /entries returns 201."""
+        """Test POST /entries returns 201 and returns Location of new entry."""
         response = self.client.post(
             '/entries',
             data=json.dumps(dict(name='Joe')), content_type='application/json')
         self.assertEqual(response.status_code, 201)
+        location = response['location']
+        location_url = urlparse.urlparse(location)
+        response = self.client.get(location_url.path)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        data = json.loads(response.content)
+        self.assertEqual(data['name'], 'Joe')
