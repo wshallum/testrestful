b6cd4b3a7a7b2c51f9606d739f58b836c23be40c
William <william@shallum.net>
2015-05-21 11:19:57 +0000

    flake8 cleanup

--- be_tests/test_add_and_get_entries.py
+++ be_tests/test_add_and_get_entries.py
@@ -1,7 +1,7 @@
-import unittest
 import json
-from base_be_test import BaseBackendTest
 import requests
+from base_be_test import BaseBackendTest
+
 
 class TestAddAndGetEntries(BaseBackendTest):
 
@@ -9,10 +9,13 @@ class TestAddAndGetEntries(BaseBackendTest):
         # First the user sees no entries when he looks at the list of all
         # entries
         all_entries = requests.get(self.url('/entries'))
-        self.assertEqual(all_entries.status_code, 200, "GET /entries returns 200")
+        self.assertEqual(
+            all_entries.status_code, 200, "GET /entries returns 200")
         # The user then adds an entry
-        add_result = requests.post(self.url('/entries'), json.dumps(dict(name="Joe")))
-        self.assertEqual(add_result.status_code, 201, "POST /entries to create returns 201")
+        add_result = requests.post(
+            self.url('/entries'), json.dumps(dict(name="Joe")))
+        self.assertEqual(
+            add_result.status_code, 201, "POST /entries to create returns 201")
         # The user checks that the entry is there when he lists all entries
         all_entries = requests.get(self.url('/entries'))
         self.assertIn("Joe", all_entries.content)
