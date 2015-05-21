def0466fed4931bbd4b64fc6c32bb0828141ba3a
William <william@shallum.net>
2015-05-21 11:25:54 +0000

    include urls from phonebook_be

--- be/django/phonebook/phonebook/urls.py
+++ be/django/phonebook/phonebook/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import phonebook_be.urls
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'phonebook.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include(phonebook_be.urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
