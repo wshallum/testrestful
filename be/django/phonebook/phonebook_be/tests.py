8ed5a676ea1c740a058e30783c9fa3e0e4decb51
William <william@shallum.net>
2015-05-21 13:54:52 +0000

    some django tests for GET /entries

--- be/django/phonebook/phonebook_be/tests.py
+++ be/django/phonebook/phonebook_be/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import json
 
-# Create your tests here.
+
+class EntriesListTests(TestCase):
+
+    def test_entries_list(self):
+        """Test GET /entries returns 200 with JSON content."""
+        response = self.client.get('/entries')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        json.loads(response.content)
--- be/django/phonebook/phonebook_be/urls.py
+++ be/django/phonebook/phonebook_be/urls.py
@@ -1,2 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+
 urlpatterns = [
+    url(r'^entries$', views.entries),
 ]
