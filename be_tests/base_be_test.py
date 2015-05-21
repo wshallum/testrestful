0a2c436d6f76f382d9fa71226b8ddb57f5d41e8b
William <william@shallum.net>
2015-05-21 11:02:35 +0000

    Initial stupid test: get, add, get

---
+++ be_tests/base_be_test.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+import urllib
+
+class BaseBackendTest(unittest.TestCase):
+    def __init__(self, method_name):
+        super(BaseBackendTest, self).__init__(method_name)
+        self.backend_base_url = os.getenv('PHONEBOOK_BACKEND_BASE_URL')
+        if self.backend_base_url is None:
+            raise KeyError, "No PHONEBOOK_BACKEND_BASE_URL in os environment"
+    def url(self, path, params = None):
+        if params is None:
+            params = dict()
+        result = self.backend_base_url + path
+        if len(params) > 0:
+            result = result + '?' + urllib.urlencode(params)
+        return result
---
+++ be_tests/test_add_and_get_entries.py
@@ -0,0 +1,19 @@
+import unittest
+import json
+from base_be_test import BaseBackendTest
+import requests
+
+class TestAddAndGetEntries(BaseBackendTest):
+
+    def test_add_and_get_entries(self):
+        # First the user sees no entries when he looks at the list of all
+        # entries
+        all_entries = requests.get(self.url('/entries'))
+        self.assertEqual(all_entries.status_code, 200, "GET /entries returns 200")
+        # The user then adds an entry
+        add_result = requests.post(self.url('/entries'), json.dumps(dict(name="Joe")))
+        self.assertEqual(add_result.status_code, 201, "POST /entries to create returns 201")
+        # The user checks that the entry is there when he lists all entries
+        all_entries = requests.get(self.url('/entries'))
+        self.assertIn("Joe", all_entries.content)
+        self.assertEqual(all_entries.status_code, 200)
